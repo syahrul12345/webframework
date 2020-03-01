fa83ad37ad95b54524a8449f3f69061ad6ea942b
syahrul12345 <muhdsyahrulnizam123@gmail.com>
2020-03-01 17:41:26 +0800

    moved login logic to redux

--- website/src/screens/login/login.js
+++ website/src/screens/login/login.js
@@ -1,19 +1,9 @@
 import React, { useState,useEffect } from 'react';
 import { Grid } from "@material-ui/core";
-import { useCookies } from 'react-cookie';
 import LoginForm from '../../components/LoginForm';
 
 const LoginPage = (props) => {
     const { redirect } = props
-    const [myCookie,setMyCookie] = useState('')
-    const [cookies, setCookie, removeCookie] = useCookies(['cookie-name']);
-    useEffect(()=> {
-      // Prevent routing on the first load
-      // The cookie will be set from the login form.
-      if (myCookie !== '') {
-        setCookie('x-token',`bearer ${myCookie}`)
-      }
-    },[cookies,myCookie])
 
     return(
       <Grid
